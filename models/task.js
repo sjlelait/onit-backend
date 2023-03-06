7fdae63cdceeaaa1030951dccbfafd41fe018710
Sara Le Lait <sjlelait@gmail.com>
2023-03-05 17:51:07 -0700

    wip: mongoDB connected

--- models/task.js
+++ models/task.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-
-const taskSchema = new mongoose.Schema({
+const taskSchema = new Schema({
     title: { type: String, required: true },
     category: { type: String, required: true },
-    timeframe: Int32,
-    priority: Int32,
+    timeframe: Number,
+    priority: Number,
     complete: Boolean,
     subtask: [{ type: Schema.Types.ObjectId, ref: 'Subtask'}]
 });

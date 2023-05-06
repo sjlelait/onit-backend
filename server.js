1dea279c4766cc29f8e4ed5018d0048769c54948
Steven Medina <smedina2494@gmail.com>
2023-05-06 01:42:40 -0400

    Fix: Removes escape chars from Firebase config

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ admin.initializeApp({
     type: 'service_account',
     project_id: 'onit-f8dae',
     private_key_id: GOOGLE_PRIVATE_KEY_ID,
-    private_key: GOOGLE_PRIVATE_KEY.replace(/\n/g, ''),
+    private_key: GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
     client_email: 'firebase-adminsdk-n60hd@onit-f8dae.iam.gserviceaccount.com',
     client_id: GOOGLE_CLIENT_ID,
     auth_uri: 'https://accounts.google.com/o/oauth2/auth',
@@ -83,7 +83,6 @@ function isAuthenticated(req, res, next) {
 // mount routes
 // router for routes
 app.use(isAuthenticated, tasksRouter); // Remove isAuthenticated for testing(will not have to sign in after)
-
 
 // tell app to listen
 app.listen(PORT, () => {
